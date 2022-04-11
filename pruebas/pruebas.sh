91ce08af39f239472ac922162df9484cf4d870f4
798334 <798334@unizar.es>
2022-04-11 18:29:49 +0200

    Modificaciones para despliegue en heroku

--- pruebas/pruebas.sh
+++ pruebas/pruebas.sh
@@ -2,13 +2,20 @@
 
 #Prueba register correcto e incorrecto
 curl -X POST -d "username=prueba&passwd=prueba&email=prueba@unizar.es" http://localhost:3000/register
+echo ""
 curl -X POST -d "username=prueba&passwd=prueba&email=prueba@unizar.es" http://localhost:3000/register
+echo ""
 
 #Prueba login correcta e incorrecta(user y passwd)
 curl -X GET -d "username=prueba&passwd=prueba" http://localhost:3000/login
+echo ""
 curl -X GET -d "username=prueba2&passwd=prueba" http://localhost:3000/login
+echo ""
 curl -X GET -d "username=prueba&passwd=prueba2" http://localhost:3000/login
+echo ""
 
 #Prueba borrar correcto e incorrecto
-curl -X GET -d "username=prueba" http://localhost:3000/login/delete
-curl -X GET -d "username=prueba" http://localhost:3000/login/delete
+curl -X POST -d "username=prueba" http://localhost:3000/login/delete
+echo ""
+curl -X POST -d "username=prueba" http://localhost:3000/login/delete
+echo ""
