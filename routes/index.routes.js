2735b702c3e0992d4d613448b578de50c0591542
798334 <798334@unizar.es>
2022-04-12 23:44:46 +0200

    Arreglo fichero index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const api = require("../api/api.controller")
+const api = require("../api/api")
 
 router.post("/register", api.registerUser);
 
