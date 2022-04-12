1bcfec05fd9e8ea34a2e3aab1f155e44eb4796a7
798334 <798334@unizar.es>
2022-04-12 02:18:15 +0200

    cambios conexión bd

--- db.js
+++ db.js
@@ -5,11 +5,11 @@ connection = process.env.DATABASE_URL;
 
 const pool = new Pool({
 
-	#user: process.env.DB_USER,
-	#password: process.env.DB_PASSWD,
-	#database: process.env.DB_NAME,
-	#host: process.env.DB_HOST,
-	#port: process.env.DB_PORT,
+/*	user: process.env.DB_USER,
+	password: process.env.DB_PASSWD,
+	database: process.env.DB_NAME,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,*/
 	connection,
 	ssl: {
         rejectUnauthorized: false,
