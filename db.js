a371a8946f247688aa17685d4d79dee17731f663
798334 <798334@unizar.es>
2022-04-12 02:16:28 +0200

    cambios conexión bd

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+connection = process.env.DATABASE_URL;
+
 const pool = new Pool({
 
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWD,
-	database: process.env.DB_NAME,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT
-
+	#user: process.env.DB_USER,
+	#password: process.env.DB_PASSWD,
+	#database: process.env.DB_NAME,
+	#host: process.env.DB_HOST,
+	#port: process.env.DB_PORT,
+	connection,
+	ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 
